3537a9ac90853ffdb4fcd0b89da6904a580385ed
brian m. carlson <sandals@crustytoothpaste.net>
2024-08-09 21:29:19 +0000

    lawn-protocol: add compatibility tests for read and write requests
    We'd like to make sure that our output is compatible between older and
    newer versions of the read and write requests, so add some tests for
    that case.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- lawn-protocol/src/protocol/mod.rs
+++ lawn-protocol/src/protocol/mod.rs
@@ -552,7 +552,7 @@ pub struct DeleteChannelRequest {
     pub termination: Option<u32>,
 }
 
-#[derive(Serialize, Deserialize, Hash, Eq, PartialEq, Ord, PartialOrd, Clone)]
+#[derive(Serialize, Deserialize, Debug, Hash, Eq, PartialEq, Ord, PartialOrd, Clone)]
 #[serde(rename_all = "kebab-case")]
 pub struct ReadChannelRequest {
     pub id: ChannelID,
@@ -566,7 +566,7 @@ pub struct ReadChannelRequest {
     pub complete: bool,
 }
 
-#[derive(Serialize, Deserialize, Hash, Eq, PartialEq, Ord, PartialOrd, Clone)]
+#[derive(Serialize, Deserialize, Debug, Hash, Eq, PartialEq, Ord, PartialOrd, Clone)]
 #[serde(rename_all = "kebab-case")]
 pub struct ReadChannelResponse {
     pub bytes: Bytes,
@@ -574,7 +574,7 @@ pub struct ReadChannelResponse {
     pub offset: Option<u64>,
 }
 
-#[derive(Serialize, Deserialize, Hash, Eq, PartialEq, Ord, PartialOrd, Clone)]
+#[derive(Serialize, Deserialize, Debug, Hash, Eq, PartialEq, Ord, PartialOrd, Clone)]
 #[serde(rename_all = "kebab-case")]
 pub struct WriteChannelRequest {
     pub id: ChannelID,
@@ -586,7 +586,7 @@ pub struct WriteChannelRequest {
     pub blocking: Option<bool>,
 }
 
-#[derive(Serialize, Deserialize, Hash, Eq, PartialEq, Ord, PartialOrd, Clone)]
+#[derive(Serialize, Deserialize, Debug, Hash, Eq, PartialEq, Ord, PartialOrd, Clone)]
 #[serde(rename_all = "kebab-case")]
 pub struct WriteChannelResponse {
     pub count: u64,
@@ -1604,5 +1604,79 @@ mod tests {
             },
             b"\xa3\x69extension\x82\x58\x23foobar@test.ns.crustytoothpaste.net\xf6\x58\x26extension@test.ns.crustytoothpaste.net\xf5\x65count\x05",
         );
+    }
+
+    #[test]
+    fn deserialize_requests_rw_compatible() {
+        #[derive(Serialize, Deserialize, Debug, Hash, Eq, PartialEq, Ord, PartialOrd, Clone)]
+        #[serde(rename_all = "kebab-case")]
+        struct ReadChannelLegacyRequest {
+            pub id: ChannelID,
+            pub selector: u32,
+            pub count: u64,
+        }
+
+        let cases: &[(&[u8], &str)] = &[
+            (b"\xa3\x62id\x00\x68selector\x02\x65count\x1a\xfe\xdc\xba\x98", "legacy"),
+            (b"\xa6\x62id\x00\x68selector\x02\x65count\x1a\xfe\xdc\xba\x98\x6bstream-sync\xf6\x68blocking\xf6\x68complete\xf4", "modern"),
+        ];
+        for (b, desc) in cases {
+            assert_decode(
+                &format!("ReadChannelRequest without blocking I/O: {}", desc),
+                &super::ReadChannelRequest {
+                    id: ChannelID(0),
+                    selector: 2,
+                    count: 0xfedcba98,
+                    stream_sync: None,
+                    blocking: None,
+                    complete: false,
+                },
+                b,
+            );
+            assert_decode(
+                &format!("ReadChannelRequest (legacy): {}", desc),
+                &ReadChannelLegacyRequest {
+                    id: ChannelID(0),
+                    selector: 2,
+                    count: 0xfedcba98,
+                },
+                b,
+            );
+        }
+
+        #[derive(Serialize, Deserialize, Debug, Hash, Eq, PartialEq, Ord, PartialOrd, Clone)]
+        #[serde(rename_all = "kebab-case")]
+        struct WriteChannelLegacyRequest {
+            pub id: ChannelID,
+            pub selector: u32,
+            pub bytes: Bytes,
+        }
+
+        let cases: &[(&[u8], &str)] = &[
+            (b"\xa3\x62id\x00\x68selector\x02\x65bytes\x44\xff\xfe\xc2\xa9", "legacy"),
+            (b"\xa5\x62id\x00\x68selector\x02\x65bytes\x44\xff\xfe\xc2\xa9\x6bstream-sync\xf6\x68blocking\xf6", "modern"),
+        ];
+        for (b, desc) in cases {
+            assert_decode(
+                &format!("WriteChannelRequest without blocking I/O: {}", desc),
+                &super::WriteChannelRequest {
+                    id: ChannelID(0),
+                    selector: 2,
+                    bytes: vec![0xffu8, 0xfe, 0xc2, 0xa9].into(),
+                    stream_sync: None,
+                    blocking: None,
+                },
+                b,
+            );
+            assert_decode(
+                &format!("WriteChannelRequest (legacy): {}", desc),
+                &WriteChannelLegacyRequest {
+                    id: ChannelID(0),
+                    selector: 2,
+                    bytes: vec![0xffu8, 0xfe, 0xc2, 0xa9].into(),
+                },
+                b,
+            );
+        }
     }
 }
